d6a8008b8a71f4db042e221c78acce0ed991bef1
tharunvenkatesh766 <tharunvenkatesh766@gmail.com>
2026-09-20 04:37:06 +0530

    Delete run.bat

--- run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo ============================================================
-echo Starting QuantumTraffic AI (Hackathon Edition)...
-echo ============================================================
-py -m streamlit run app.py
-pause
